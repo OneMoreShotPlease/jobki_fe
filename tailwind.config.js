a3c70732f6b530ae3fa2e632a310d95ff9f7412d
hyunu0831 <hyunu.dev@gmail.com>
2024-04-08 18:59:15 +0900

    chore: tailwind 관련 파일 추가 - postcss, tailwind config 파일

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        base: { min: '320px', max: '700px' },
+      },
+      colors: {
+        green: {
+          50: '#f0fdf5',
+          100: '#dcfce8',
+          200: '#bbf7d1',
+          300: '#86efad',
+          400: '#4ade81',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803c',
+          800: '#166533',
+          900: '#14532b',
+          950: '#052e14',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
