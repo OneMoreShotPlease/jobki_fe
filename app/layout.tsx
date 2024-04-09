5637e26aadc2fd81b120be4ba56f97bdfa355187
hyunu0831 <hyunu.dev@gmail.com>
2024-04-09 15:50:01 +0900

    feat: navBar, 오른쪽 사이드 아바타 로고 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 
 // These styles apply to every route in the application
 import './globals.css';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Jobki',
@@ -14,7 +16,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <div className=" flex justify-center ">
           <div className="h-[100dvh] w-full min-w-[320px] max-w-[700px]  text-left ">
-            {children}
+            <div className="flex h-auto w-full flex-col gap-4">
+              <div className="flex h-[60px] w-full flex-row items-center justify-between pl-[16px] pr-[16px]  ">
+                <Link href="/">
+                  <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-green-600">
+                    JobKi
+                  </h4>
+                </Link>
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-green-600">
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    {/* user name 넣어주기 */}
+                    <AvatarFallback>Me</AvatarFallback>
+                  </Avatar>
+                </h4>
+              </div>
+              {children}
+            </div>
           </div>
         </div>
       </body>
