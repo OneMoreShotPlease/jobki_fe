df8342b3e935648ff2869f3a94665293ecd10002
hyunu0831 <hyunu.dev@gmail.com>
2024-04-09 15:53:05 +0900

    feat: search 관련 페이지, 컴포넌트 추가 - search/page.tsx - RecentSearch(최근 검색한 단어), RelatedTag(관련 태그), SearchResult(검색 결과)

---
+++ app/search/_components/RelatedTag.tsx
@@ -0,0 +1,10 @@
+import MainCard from '@/app/(main)/_components/MainCard';
+
+export default function RelatedTag() {
+  return (
+    <>
+      <MainCard cardTitle="관련 태그" />
+      <></>
+    </>
+  );
+}
---
+++ app/search/_components/SearchResult.tsx
@@ -0,0 +1,9 @@
+import MainCard from '@/app/(main)/_components/MainCard';
+
+export default function SearchResult() {
+  return (
+    <>
+      <MainCard cardTitle="검색 결과" />
+    </>
+  );
+}
