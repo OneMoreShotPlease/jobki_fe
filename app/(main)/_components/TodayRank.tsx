4238f43ab8519e96d43d7c1b683b3c2ae820da49
hyunu0831 <hyunu.dev@gmail.com>
2024-04-09 15:50:46 +0900

    feat: 컴포넌트 분리 작업

---
+++ app/(main)/_components/TodayRank.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Card, CardContent } from '@/components/ui/Card';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/Carousel';
+import MainCard from './MainCard';
+
+export default function TodayRank() {
+  return (
+    <MainCard
+      cardTitle="오늘의 순위"
+      cardDescription="질문/태그/유저"
+      cardFooter=""
+      cardContent={
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>
+              <Card>
+                <CardContent>1</CardContent>
+              </Card>
+            </CarouselItem>
+            <CarouselItem>
+              <Card>
+                <CardContent>2</CardContent>
+              </Card>
+            </CarouselItem>
+            <CarouselItem>
+              <Card>
+                <CardContent>3</CardContent>
+              </Card>
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
+      }
+    />
+  );
+}
