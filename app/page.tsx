d0a5f2995b7a7e0448f53d8e54b84507889597ce
hyunu0831 <hyunu.dev@gmail.com>
2024-04-09 10:55:59 +0900

    feat: home에서 테스트 했던 레이아웃 전체 레이아웃으로 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <div className="max-w-[700px] min-w-[320px] text-left">
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-green-600">JobKi</h4>
-      </div>
+    <div>
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-green-600">JobKi</h4>
     </div>
   );
 }
