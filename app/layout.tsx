cc0e19660604660e1ed0585667085743df99d39a
hyunu0831 <hyunu.dev@gmail.com>
2024-05-14 13:39:17 +0900

    fix: layout 수정 - SearchInput 컴포넌트 이동 - 전체 padding 값 조절 - Avatar Usermenu Dropdown 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/Avatar';
 import Link from 'next/link';
+import SearchInput from '@/components/common/SearchInput';
+import { UserMenu } from '@/components/common/UserMenu';
 
 export const metadata: Metadata = {
   title: 'Jobki',
@@ -14,24 +16,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <div className=" flex justify-center ">
-          <div className="h-[100dvh] w-full min-w-[320px] max-w-[700px]  text-left ">
+        <div className="flex justify-center px-4">
+          <div className="h-[100dvh] w-full min-w-[280px] max-w-[700px] text-left ">
             <div className="flex h-auto w-full flex-col gap-4">
-              <div className="flex h-[60px] w-full flex-row items-center justify-between pl-[16px] pr-[16px]  ">
+              <div className="flex h-[60px] w-full flex-row items-center justify-between px-2 ">
                 <Link href="/">
                   <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-green-600">
                     JobKi
                   </h4>
                 </Link>
-                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-green-600">
-                  <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" />
+                <UserMenu>
+                  <Avatar className="transition duration-300 ease-in-out hover:shadow-lg focus:border-none focus:outline-none dark:hover:shadow-black/30">
+                    <AvatarImage className="hover:scale-110 " src="https://github.com/shadcn.png" />
                     {/* user name 넣어주기 */}
                     <AvatarFallback>Me</AvatarFallback>
                   </Avatar>
-                </h4>
+                </UserMenu>
               </div>
-              {children}
+              <div className="flex flex-col gap-2 px-2">
+                <SearchInput />
+                {children}
+              </div>
             </div>
           </div>
         </div>
